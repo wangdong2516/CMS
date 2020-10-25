0e591b4c2b64d48db696a3dcd85743c05c0660e3
v_dongawang <v_dongawang@tencent.com>
2020-10-25 14:13:58 +0800

    modify pro_settings.py

--- settings/dev_settings.py
+++ settings/dev_settings.py
@@ -2,3 +2,19 @@ from settings.pro_settings import *
 
 DEBUG = True
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CMS',
+        'USER': 'root',
+        'PASSWORD': '1277431229',
+        'PORT': 3306,
+        # 指定存储引擎
+        'STORAGE_ENGINE': 'INNODB',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # 设置sql模式为严格模式，这种情况下，一些警告将会升级为错误
+            'charset': 'utf8mb4'  # 指定使用的字符集
+        }
+    }
+}
