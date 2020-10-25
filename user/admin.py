5bf34213b75df0262e9750af9efaa29fe84c8cda
v_dongawang <v_dongawang@tencent.com>
2020-10-25 13:53:17 +0800

    add poetry index and admin site

--- user/admin.py
+++ user/admin.py
@@ -5,7 +5,23 @@ from user.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'source_type', 'user_type')
+    # 列表页面展示的数据
+    list_display = ('username', 'email', 'source_type', 'user_type', 'date_joined')
+    # 用作搜索的字段
+    search_fields = ('user_type', 'username')
+
+    # 对结果进行过滤
+    list_filter = ('username',)
+
+    # 可能回遇到mysql时区的问题，需要手动导入时区表
+    # mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p mysql
+    date_hierarchy = 'date_joined'
+
+    # 指定排序，效果等同于Meta属性中的ordering
+    ordering = ('date_joined', )
+
+    # 详情页面展示的字段
+    fields = ('username', 'date_joined', 'email', 'source_type', 'user_type')
 
 
 admin.site.register(User, UserAdmin)
