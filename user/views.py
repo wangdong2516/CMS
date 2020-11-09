fa76c42792a7821b9bf85b46ed6d3dd6d0e2a617
v_dongawang <v_dongawang@tencent.com>
2020-11-09 22:13:17 +0800

    add jwt token

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from user.serializers import UserRegisterSerializer
 from django.views import View
 # Create your views here.
@@ -15,3 +16,7 @@ class UserRegirestView(CreateAPIView):
     """用户注册视图"""
 
     serializer_class = UserRegisterSerializer
+
+
+class UserLoginView(APIView):
+    """用户登录视图"""
